c0b6e36a37519f70b05975b0cd112897601038e1
Kapil_Kapil <kapiljlpt@gmail.com>
2026-06-10 16:09:25 +0530

    Update Data_types.cs

--- C#.cs/Data_types.cs
+++ C#.cs/Data_types.cs
@@ -18,39 +18,6 @@ Syntax
 */
 ===================================================================================================================
 
-
-✅🔥🚀C# Variables:
-Variables are containers for storing data values.
-
-In C#, there are different types of variables (defined with different keywords), for example:
-int - stores integers (whole numbers), without decimals, such as 123 or -123
-double - stores floating point numbers, with decimals, such as 19.99 or -19.99
-char - stores single characters, such as 'a' or 'B'. Char values are surrounded by single quotes
-string - stores text, such as "Hello World". String values are surrounded by double quotes
-bool - stores values with two states: true or false
-
-Examples:
-int myNum = 5;
-double myDoubleNum = 5.99D;
-char myLetter = 'D';
-bool myBool = true;
-string myText = "Hello";
-
-===================================================================================================================
-
-✅🔥C# Identifiers
-All C# variables must be identified with unique names.
-These unique names are called identifiers.
-
-Identifiers can be short names (like x and y) or more descriptive names (age, sum, totalVolume).
-Note: It is recommended to use descriptive names in order to create understandable and maintainable code:
-
-Example: Get your own C# Server
-int minutesPerHour = 60; // Good
-int m = 60;  // OK, but not so easy to understand what m actually is
-
-==================================================================================================================
-
 ✅🔥🎯 What is a Data Type?
 
 A Data Type tells the compiler:
