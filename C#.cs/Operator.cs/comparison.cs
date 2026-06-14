85a0acbd1aa09ab26ea3a99fc0ebbcbea84f2c57
Kapil_Kapil <kapiljlpt@gmail.com>
2026-06-14 11:39:29 +0530

    Create comparison.cs

---
+++ C#.cs/Operator.cs/comparison.cs
@@ -0,0 +1,15 @@
+✅🔥 Comparison operators (also called Relational Operators) are used to compare two values or expressions.
+
+The result of every comparison operator is always a bool value:
+true
+false
+These operators are commonly used with if, else, while, for, and other decision-making statements.
+
+| **Operator** | **Meaning**              | **Expression** | **Small Code**                 |
+| ------------ | ------------------------ | -------------- | ------------------------------ |
+| `==`         | Equal to                 | `a == b`       | `Console.WriteLine(10 == 10);` |
+| `!=`         | Not equal to             | `a != b`       | `Console.WriteLine(10 != 20);` |
+| `>`          | Greater than             | `a > b`        | `Console.WriteLine(20 > 10);`  |
+| `<`          | Less than                | `a < b`        | `Console.WriteLine(10 < 20);`  |
+| `>=`         | Greater than or equal to | `a >= b`       | `Console.WriteLine(20 >= 20);` |
+| `<=`         | Less than or equal to    | `a <= b`       | `Console.WriteLine(10 <= 20);` |
