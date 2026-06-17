a360a6047e1bdf32392d497b176dfe90d51a32ef
Kapil_Kapil <kapiljlpt@gmail.com>
2026-06-17 11:35:27 +0530

    Create Unary.cs

---
+++ C#.cs/Operator.cs/Unary.cs
@@ -0,0 +1,16 @@
+✅🔥Unary Operators in C#:
+A Unary Operator is an operator that works on only one operand (one variable or one value).
+For example:
+int x = 10;
+x++;
+Here, ++ operates on only one operand (x), so it is a unary operator.
+
+
+| **Operator** | **Meaning**            | **Expression** | **Small Code**                       |
+| ------------ | ---------------------- | -------------- | ------------------------------------ |
+| `+`          | Unary Plus             | `+x`           | `Console.WriteLine(+10);`            |
+| `-`          | Unary Minus (Negation) | `-x`           | `Console.WriteLine(-10);`            |
+| `++`         | Increment              | `x++` or `++x` | `int x = 5; Console.WriteLine(++x);` |
+| `--`         | Decrement              | `x--` or `--x` | `int x = 5; Console.WriteLine(--x);` |
+| `!`          | Logical NOT            | `!x`           | `Console.WriteLine(!true);`          |  Reverses a Boolean value.
+| `~`          | Bitwise Complement     | `~x`           | `Console.WriteLine(~5);`             |  Flips every bit.~x = -(x + 1)
