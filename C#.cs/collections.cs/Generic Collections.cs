8e89ab2f778c2e96a7a13059f86f0c6412a6b312
Kapil_Kapil <kapiljlpt@gmail.com>
2026-06-21 15:20:52 +0530

    Create Generic Collections.cs

---
+++ C#.cs/collections.cs/Generic Collections.cs
@@ -0,0 +1,33 @@
+✅🔥Generic Collections (Modern & Preferred)
+
+Namespace:
+using System.Collections.Generic;
+
+Features:
+    Type-safe
+    No boxing/unboxing
+    Faster performance
+    Compile-time checking
+
+Example:
+List<int> numbers = new List<int>();
+numbers.Add(10);
+numbers.Add(20);
+numbers.Add(30);
+Strong Type Safety: numbers.Add("Hello"); // ❌ Error
+
+
+
+✅🔥Common Generic Collections:
+| Collection              | Type           |
+| ----------------------- | -------------- |
+| List<T>                 | Dynamic array  |
+| Dictionary<TKey,TValue> | Key-value pair |
+| Queue<T>                | FIFO           |
+| Stack<T>                | LIFO           |
+| HashSet<T>              | Unique values  |
+
+
+
+
+
